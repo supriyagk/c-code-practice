209e6e415426634fb32d8411063af17dfb5231e8
supriyagk <gksupriya09@gmail.com>
2024-03-24 20:59:47 +0530

    ver 0.4

---
+++ format specifier 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int num1 = 10;
+    float num2 = 20.99;
+    char letter = 'A';
+
+    printf ("print the num1 is: %d\n", num1);
+    printf ("print the num2 is: %f\n", num2);
+    printf ("print the letter is: %c\n", letter);
+    return 0;
+}
