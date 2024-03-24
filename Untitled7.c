448028c100654f47ab8af8b5c30c80ccfc6fc55e
supriyagk <gksupriya09@gmail.com>
2024-03-24 19:55:36 +0530

    ver 0.3

---
+++ Untitled7.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int myNum;
+    printf ("enter a number:");
+    scanf("%d", &myNum);
+    printf("Print the number: %d\n", myNum);
+    return 0;
+}
---
+++ format specifier %d.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int myNum;
+    myNum = 50;
+    printf ("print my number: %d",myNum);
+    return 0;
+}
